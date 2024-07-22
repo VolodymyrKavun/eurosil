012aaf81fda64ad6b4ed27af11037174c50a8656
Nataliia <natalabojko@gmail.com>
2024-07-22 19:45:03 +0300

    responsive InnerItemGaps

--- src/components/CatalogueList/CatalogueListHome.jsx
+++ src/components/CatalogueList/CatalogueListHome.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import Image from 'next/image'
+import Image from "next/image";
 
 import styles from "./CatalogueList.module.scss";
 import { areas } from "@/data/areas";
@@ -10,39 +10,39 @@ const CatalogueListHome = () => {
   return (
     <ul className={styles.catalogueList}>
       {areas
-          ?.filter(
-            (item) =>
-              item.category === "medicine" ||
-              item.category === "cosmetology" ||
-              item.category === "agricultural" ||
-              item.category === "construction"
-          )
-          .map(({ title, category, image }) => (
-            <li key={title} className={styles.catalogueItem}>
-              <Link
-                href={`/catalogue/${category}`}
-                className={styles.catalogueLink}
-              >
-                <div className={styles.imgBox}>
-                  <Image
-                    src={image}
-                    alt="фото сайту"
-                    fill={true}
-                    priority={true}
-                    className={styles.img}
-                    sizes="(max-width: 767px) 100vw, 50vw"
-                  />
-                </div>
-                <div className={styles.subtitleBox}>
-                  <h3 className={styles.subtitle}>{title}</h3>
+        ?.filter(
+          (item) =>
+            item.category === "medicine" ||
+            item.category === "cosmetology" ||
+            item.category === "agricultural" ||
+            item.category === "construction"
+        )
+        .map(({ title, category, image }) => (
+          <li key={title} className={styles.catalogueItem}>
+            <Link
+              href={`/catalogue/${category}`}
+              className={styles.catalogueLink}
+            >
+              <div className={styles.imgBox}>
+                <Image
+                  src={image}
+                  alt="фото сайту"
+                  fill={true}
+                  priority={true}
+                  className={styles.img}
+                  sizes="(max-width: 767px) 100vw, 50vw"
+                />
+              </div>
+              <div className={styles.subtitleBox}>
+                <h3 className={styles.subtitle}>{title}</h3>
 
-                  <svg className={styles.icon}>
-                    <use href="./sprite.svg#arrow-right"></use>
-                  </svg>
-                </div>
-              </Link>
-            </li>
-          ))}
+                <svg className={styles.icon}>
+                  <use href="./sprite.svg#arrow-right"></use>
+                </svg>
+              </div>
+            </Link>
+          </li>
+        ))}
     </ul>
   );
 };
--- src/sections/homeProductsSection/HomeProductsSection.jsx
+++ src/sections/homeProductsSection/HomeProductsSection.jsx
@@ -1,29 +1,26 @@
-import ProductsList from '@/components/ProductsList/ProductsList'
-import FreeSamples from '@/components/FreeSamples/FreeSamples'
-import BtnLink from '@/components/buttons/BtnLink/BtnLink'
-import { products } from '@/data/products'
-import styles from './HomeProductsSection.module.scss'
-
+import ProductsList from "@/components/ProductsList/ProductsList";
+import FreeSamples from "@/components/FreeSamples/FreeSamples";
+import BtnLink from "@/components/buttons/BtnLink/BtnLink";
+import { products } from "@/data/products";
+import styles from "./HomeProductsSection.module.scss";
 
 const HomeProductsSection = () => {
+  return (
+    <section>
+      <div className={`container ${styles.container}`}>
+        <h2 className={`sectionTitle ${styles.produceTitle}`}>
+          <span>Продукція</span>
+        </h2>
+        <ProductsList data={products} />
+        <FreeSamples />
+        <BtnLink
+          title="Вся продукція"
+          href="/products"
+          className={styles.allProduceBtn}
+        />
+      </div>
+    </section>
+  );
+};
 
-
-    return (
-        <section>
-            <div className={`container ${styles.container}`}>
-                <h1 className={styles.produceTitle}>Продукція</h1>
-                <ProductsList data={products}
-                />
-                <FreeSamples />
-                <BtnLink
-                    title="Вся продукція"
-                    href="/products"
-                    className={styles.allProduceBtn}
-                />
-            </div>
-        </section>
-    )
-}
-
-
-export default HomeProductsSection
+export default HomeProductsSection;
